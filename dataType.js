a787e702e286e36c6d57151840f264c1a6c7e14b
Bagus Adi Saputra <bagusadisaputra04@gmail.com>
2023-11-08 11:26:03 +0700

    Belajar Data Type di JavaScript

---
+++ dataType.js
@@ -0,0 +1,21 @@
+// Belajar Type Data pada JavaScript
+
+// KET: (typeof) Untuk mengetahui jenis type data
+
+const string = 'Bagus Adi Saputra'; // Type Data String
+console.log(typeof string);
+
+const number = 24; // Type Data Number
+console.log(typeof number);
+
+const boolean = true; // Type Data Number
+console.log(typeof boolean);
+
+const array = []; // Type Data Array atau Object
+console.log(typeof array);
+
+const typeUndefined = undefined; // Type Data Undefined
+console.log(typeof typeUndefined);
+
+const object = null; // Type Data Object
+console.log(typeof object);
